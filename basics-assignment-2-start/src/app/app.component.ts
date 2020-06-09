b5da58318633654c2e870ee1c7bc6ae751d8f48e
Sam Younger <sam.younger@gmail.com>
2020-06-09 23:29:06 +0100

    completed assignment 2

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -6,4 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public username: string;
+  public buttonDisabled: boolean = true;
+
+  onTextChange(): void {
+    if(this.username.length > 0) {
+      this.buttonDisabled = false;
+    } else {
+      this.buttonDisabled = true;
+    }
+  }
+
+  onButtonClick(): void {
+    this.username = "";
+    this.onTextChange();
+  }
 }
