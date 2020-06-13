ff026900a619c03fc71baea6662f8fb5406f9cea
samyounger <sam.younger@gmail.com>
2020-06-13 17:33:27 +0100

    completed stage 1 of project-1

--- project-1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      "A test recipe",
+      "This is simply a test",
+      "https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg"
+    )
+  ];
 }
