a6e00dca714720090b8dc0cfd6462d666766a402
Sam Younger <sam.younger@gmail.com>
2020-06-17 23:36:54 +0100

    progress in project 1

--- project-1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -6,11 +6,22 @@ import { Recipe } from '../recipe.model';
   templateUrl: './recipe-list.component.html',
 })
 export class RecipeListComponent {
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       "A test recipe",
       "This is simply a test",
       "https://www.inspiredtaste.net/wp-content/uploads/2018/12/Sauteed-Zucchini-Recipe-1-1200.jpg"
+    ),
+    new Recipe(
+      "Another test recipe",
+      "This is simply a test, mark two",
+      "https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg"
     )
   ];
+
+  onRecipeSelected(recipe: Recipe): void {
+    this.recipeSelected.emit(recipe);
+  }
 }
--- project-1/src/app/recipes/recipes.component.ts
+++ project-1/src/app/recipes/recipes.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
 })
 export class RecipesComponent {
+  public recipeSelected: Recipe;
+
+  onRecipeSelected(recipe: Recipe): void {
+    this.recipeSelected = recipe;
+  }
 }
