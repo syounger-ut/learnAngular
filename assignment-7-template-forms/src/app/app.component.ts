edd5b7c8b193a802af4d909011250a5c8d68440b
Sam Younger <sam.younger@gmail.com>
2020-07-02 22:57:57 +0100

    completed template driven form assignment

--- assignment-7-template-forms/src/app/app.component.ts
+++ assignment-7-template-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('loginForm', { static: true }) public form: NgForm;
+  public subscriptions: string[] = ["Basic", "Advanced", "Pro"];
+  public selectedSubscription: string = "Advanced";
 
+  onSubmit(): void {
+    console.log(this.form.value);
+  }
 }
