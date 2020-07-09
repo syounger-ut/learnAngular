e2fac7cb4944746e0c96130230e432133479fa33
Sam Younger <sam.younger@gmail.com>
2020-07-09 22:56:05 +0100

    completed reactive-forms assignment

--- assignment-8-reactive-forms/src/app/app.component.ts
+++ assignment-8-reactive-forms/src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+  ValidatorFn,
+  AbstractControl,
+} from '@angular/forms';
+import { CustomValidators } from './custom-validators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public projectForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.projectForm = this.formBuilder.group({
+      projectName: ['', [Validators.required, CustomValidators.invalidProjectName], CustomValidators.asyncInvalidProjectName],
+      mail: ['', [Validators.required, Validators.email]],
+      projectStatus: ['critical'],
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.projectForm);
+  }
 }
