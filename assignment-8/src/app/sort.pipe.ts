df08991b89179b32d494c5096d004daa5e369fb0
samyounger <sam.younger@gmail.com>
2020-09-15 22:46:33 +0100

    assignment 8

---
+++ assignment-8/src/app/sort.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort',
+  pure: false,
+})
+export class SortPipe implements PipeTransform {
+  transform(value: [], sortBy: string): any {
+    if (value.length === 0) {
+      return value;
+    }
+
+    return value.sort((a, b) => {
+      if (a[sortBy] < b[sortBy]) {
+        return -1;
+      }
+      if (a[sortBy] > b[sortBy]) {
+        return 1;
+      }
+      return 0;
+    });
+  }
+}
